6c9c81d5168ef6f1dcdbf5422752e3772b033d11
Manu Garg <manu.garg@classicinformatics.com>
2024-08-02 13:01:10 +0530

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './Components/NavBar';
 
 function App() {
   return <Grid templateAreas={{
     base: `"nav main"`, // For defining mobile devices screen layout
     lg: `"nav nav" "aside main"` // For defining larger screen layout, We want aside panel to see on the larger devices with resolution 1024 px
   }}>
-    <GridItem area='nav' bg='coral' >Nav</GridItem>
+    <GridItem area='nav'>
+      <NavBar></NavBar>
+    </GridItem>
     <Show above="lg">
       {/* to hide the aside at smaller screens */}
       <GridItem area='aside' bg='gold' >Aside</GridItem>
