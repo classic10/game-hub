1e490518a9029e8a3b8e84f28372c4ba4f375817
Manu Garg <manu.garg@classicinformatics.com>
2024-08-04 13:08:27 +0530

    implement icons in game card

--- src/Components/GameCard.tsx
+++ src/Components/GameCard.tsx
@@ -1,5 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconsList from "./PlatformIconsList";
+
 
 interface Props {
     game: Game
@@ -11,6 +13,7 @@ const GameCard = ({ game }: Props) => {
             <Image src={game.background_image}></Image>
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
+                <PlatformIconsList platforms={game.parent_platforms.map(p => p.platform)}></PlatformIconsList>
             </CardBody>
         </Card >
     )
---
+++ src/Components/PlatformIconsList.tsx
@@ -0,0 +1,36 @@
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { icons, IconType } from 'react-icons';
+
+interface Props {
+    platforms: Platform[]
+}
+
+const PlatformIconsList = ({ platforms }: Props) => {
+    const iconMap: { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        android: FaAndroid,
+        ios: MdPhoneIphone,
+        web: BsGlobe
+    }
+    return (
+        <>
+            {/*platforms.map(platform => <Text>{platform.name} </Text>)*/}
+            <HStack marginY={1}> {/* Theme spacing in chakra by default is 4 we are making this 1 */}
+                <Icon as={iconMap[platforms.slug]}></Icon>
+                {platforms.map(platform => <Icon as={iconMap[platform.slug]} color='gray.500'> </Icon>)}
+            </HStack>
+        </>
+    );
+}
+
+export default PlatformIconsList; 
