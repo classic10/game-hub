8ee7994e7082b185b63b6690dbd69b9be1ac2e30
Manu Garg <manu.garg@classicinformatics.com>
2024-08-04 15:44:12 +0530

    Refactor common css code

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 
 
@@ -13,8 +14,14 @@ const GameGrid = () => {
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding='10px'>
-                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}></GameCardSkeleton>)}
-                {/*games.map(game => <GameCard key={game.id} game={game}></GameCard>)*/}
+                {isLoading && skeletons.map((skeleton) => (
+                    <GameCardContainer>
+                        <GameCardSkeleton key={skeleton}>
+
+                        </GameCardSkeleton>
+                    </GameCardContainer>
+                ))}
+                {games.map(game => <GameCardContainer><GameCard key={game.id} game={game}></GameCard></GameCardContainer>)}
             </SimpleGrid>
         </>
     );
