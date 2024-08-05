4a8ee36b2cd03c8d707b870d1d99e9031bf9295a
Manu Garg <manu.garg@classicinformatics.com>
2024-08-05 12:48:16 +0530

    handle no image url in api

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
+import noImage from '../assets/no-image-placeholder.webp';
 
 const getCroppedImageUrl = (url: string) => {
-    if (url == null) return '';
+    if (url == null) return noImage;
     const target = 'media/';
     var index = url.indexOf(target) + target.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
